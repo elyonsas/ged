11b99d6e376da64cbebfe8fdf65b66d56817b4d3
Arnaud <arnaudadjovi274@gmail.com>
2022-11-04 16:09:36 +0100

    fix #6

--- roll/ag/view_redirect/index.php
+++ roll/ag/view_redirect/index.php
@@ -2,7 +2,17 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/ged/db.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/ged/fonctions.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/ged/fonctions-sql.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/ged/vendor/autoload.php');
 
 connected('ag');
 
+if (isset($_GET['action'])) {
 
+    if ($_GET['action'] == 'view_collaborateur') {
+
+        $id_collaborateur = $_GET['id_view_collaborateur'];
+        $_SESSION['id_view_collaborateur'] = $id_collaborateur;
+
+        header('Location: /ged/roll/ag/collaborateurs/view');
+    }
+}
