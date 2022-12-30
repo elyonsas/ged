c1572905867606a97b6f65235cc5fab91add68b0
Arnaud <arnaudadjovi274@gmail.com>
2022-12-30 15:49:21 +0100

    Add online png to connexion page

--- index.php
+++ index.php
@@ -61,7 +61,7 @@ License: For each use you must have a valid license purchased only from above li
 	<meta property="og:url" content="https://keenthemes.com/metronic" />
 	<meta property="og:site_name" content="Keenthemes | Metronic" />
 	<link rel="canonical" href="https://preview.keenthemes.com/metronic8" />
-	<link rel="shortcut icon" href="https://elyonsas.github.io/ged-assets/assets/media/logos/ged-logo-mini.png" />
+	<link rel="shortcut icon" href="https://elyonsas.github.io/ged-assets/assets/media/logos/ged-icon.png" />
 	<!--begin::Fonts-->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700" />
 	<!--end::Fonts-->
