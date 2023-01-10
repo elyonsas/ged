e765c25752ac978aa3fc1f0deca3e1ccb72ac238
Arnaud <arnaudadjovi274@gmail.com>
2023-01-10 16:02:16 +0100

    Entrain de mettre en place les param pour l'exportation en pdf

--- roll/ag/dossiers/docs/export/index.php
+++ roll/ag/dossiers/docs/export/index.php
@@ -13,6 +13,11 @@ use Dompdf\Options;
 use Knp\Snappy\Pdf;
 
 $id_document = $_GET['id_document'];
+$header_html = $_GET['header_html'];
+$footer_html = $_GET['footer_html'];
+$bg_graphique = $_GET['bg_graphique'];
+$filigrane = $_GET['filigrane'];
+$mode = $_GET['mode'];
 
 $query = "SELECT * FROM document WHERE id_document = $id_document";
 $statement = $db->prepare($query);
@@ -23,7 +28,6 @@ $table_document = $result['table_document'];
 $type_document = $result['type_document'];
 
 $snappy = new Pdf($_SERVER['DOCUMENT_ROOT'] . '/ged/assets/plugins/custom/wkhtmltopdf/bin/wkhtmltopdf');
-// $snappy = new Pdf($_SERVER['DOCUMENT_ROOT'] . '/ged/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64');
 
 $snappy->setOption('encoding', 'UTF-8');
 $snappy->setOption('image-quality', 100);
@@ -31,18 +35,30 @@ $snappy->setOption('enable-local-file-access', true);
 
 $snappy->setOption('title', $result['titre_document']);
 
+if ($mode == 'portrait') {
+  $snappy->setOption('orientation', 'Portrait');
+} else if ($mode == 'paysage') {
+  $snappy->setOption('orientation', 'Landscape');
+}
+
 if ($type_document == 'generate') {
 
   $query = "SELECT * FROM document, $table_document WHERE document.id_document = $table_document.id_document AND $table_document.id_document = $id_document";
   $statement = $db->prepare($query);
   $statement->execute();
   $result = $statement->fetch();
+
+  if ($bg_graphique == 'non') {
+    $css = 'background-color: #fff !important;';
+  }
+
   // html style
   $htmlTemplate = <<<HTML
       <style>
         *{
           max-height: 100% !important;
           white-space: normal !important;
+          {$css}
         }
         body{
           color: #000;
@@ -54,31 +70,35 @@ if ($type_document == 'generate') {
   // html content
   $htmlTemplate .= $result['contenu_document'];
 
+  if ($header_html == 'oui') {
+    $snappy->setOption('header-html', 'header.html');
+  }
+  if ($footer_html == 'oui') {
+    $snappy->setOption('footer-html', 'footer.html');
+  }
 
-  // $snappy->setOption('margin-top', '25mm');
-  // $snappy->setOption('margin-right', '15mm');
-  // $snappy->setOption('margin-bottom', '25mm');
-  // $snappy->setOption('margin-left', '15mm');
 
-  $snappy->setOption('footer-font-size', 10);
-
-  $snappy->setOption('footer-html', 'footer.html');
-  $snappy->setOption('header-html', 'header.html');
 } else if ($type_document == 'write') {
 
   $query = "SELECT * FROM document, $table_document WHERE document.id_document = $table_document.id_document AND $table_document.id_document = $id_document";
   $statement = $db->prepare($query);
   $statement->execute();
   $result = $statement->fetch();
+
+  if ($bg_graphique == 'non') {
+    $css = 'background-color: #fff !important;';
+  }
+
   // html style
   $htmlTemplate = <<<HTML
     <style>
       body{
         color: #000;
         padding: 0px 15px;
+        {$css}
       }
       li{
-          white-space: nowrap !important;
+        white-space: nowrap !important;
       }
     </style>
   HTML;
@@ -86,16 +106,14 @@ if ($type_document == 'generate') {
   // html content
   $htmlTemplate .= $result['contenu_document'];
 
-  // $snappy->setOption('margin-top', '25mm');
-  // $snappy->setOption('margin-right', '15mm');
-  // $snappy->setOption('margin-bottom', '25mm');
-  // $snappy->setOption('margin-left', '15mm');
-
-  $snappy->setOption('footer-font-size', 10);
   $snappy->setOption('minimum-font-size', 20);
 
-  $snappy->setOption('footer-html', 'footer.html');
-  $snappy->setOption('header-html', 'header.html');
+  if ($header_html == 'oui') {
+    $snappy->setOption('header-html', 'header.html');
+  }
+  if ($footer_html == 'oui') {
+    $snappy->setOption('footer-html', 'footer.html');
+  }
 }
 
 // echo $htmlTemplate;
