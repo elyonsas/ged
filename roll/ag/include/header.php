70efa2bc08ca066ace5cfc75e9e664c38380ff17
Arnaud <arnaudadjovi274@gmail.com>
2023-01-12 15:25:04 +0100

    correction page param

--- roll/ag/include/header.php
+++ roll/ag/include/header.php
@@ -214,7 +214,13 @@
                 <div class="d-flex align-items-center ms-1 ms-lg-3" id="kt_header_user_menu_toggle">
                     <!--begin::Menu wrapper-->
                     <div class="cursor-pointer symbol symbol-30px symbol-md-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-attach="parent" data-kt-menu-placement="bottom-end">
-                        <img src="assets/media/avatars/<?= user('avatar') ?>" alt="user">
+                    <?php 
+                        $avatar = select_info('avatar_utilisateur', 'utilisateur', "id_utilisateur = {$_SESSION['id_utilisateur']}", $db);
+                        $nom = select_info('nom_utilisateur', 'utilisateur', "id_utilisateur = {$_SESSION['id_utilisateur']}", $db);
+                        $prenom = select_info('prenom_utilisateur', 'utilisateur', "id_utilisateur = {$_SESSION['id_utilisateur']}", $db);
+                        $email = select_info('email_utilisateur', 'utilisateur', "id_utilisateur = {$_SESSION['id_utilisateur']}", $db);
+                    ?>    
+                    <img src="assets/media/avatars/<?= $avatar ?>" alt="user">
                     </div>
                     <!--begin::User account menu-->
                     <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px" data-kt-menu="true">
@@ -223,15 +229,15 @@
                             <div class="menu-content d-flex align-items-center px-3">
                                 <!--begin::Avatar-->
                                 <div class="symbol symbol-50px me-5">
-                                    <img alt="Logo" src="assets/media/avatars/<?= user('avatar') ?>">
+                                    <img alt="Logo" src="assets/media/avatars/<?= $avatar ?>">
                                 </div>
                                 <!--end::Avatar-->
                                 <!--begin::Username-->
                                 <div class="d-flex flex-column">
-                                    <div class="fw-bold d-flex align-items-center fs-5"><?= user('prenom') . ' ' . user('nom') ?>
+                                    <div class="fw-bold d-flex align-items-center fs-5"><?= $prenom . ' ' . $nom ?>
                                         <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>
                                     </div>
-                                    <a href="mailto:" class="fw-semibold text-muted text-hover-primary fs-7"><?= user('email') ?></a>
+                                    <a href="mailto:" class="fw-semibold text-muted text-hover-primary fs-7"><?= $email ?></a>
                                 </div>
                                 <!--end::Username-->
                             </div>
