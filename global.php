92f8593db0ad41196ec84290d53e915ee703b02a
Arnaud <arnaudadjovi274@gmail.com>
2023-02-04 13:34:25 +0100

    global.php modif

--- global.php
+++ global.php
@@ -1,10 +1,10 @@
 <?php
     date_default_timezone_set('Africa/Porto-Novo');
     setlocale(LC_TIME, "fr_FR", "French");
-    define('DB_NAME', 'ged');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
+    define('DB_NAME', 'u333556793_ged');
+    define('DB_USER', 'u333556793_root');
+    define('DB_PASSWORD', 'Arn5551666*');
     define('DB_HOST', 'localhost');
 
-    define('GOOGLE_ID', '174501764379-dik5a8upbjtqm9ae11gj2ds2sgcu5lia.apps.googleusercontent.com');
-    define('GOOGLE_SECRET', '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');
+    define('GOOGLE_ID', '174501764379-3oqunv0vsrncbe81ekcvb910omj43s6f.apps.googleusercontent.com');
+    define('GOOGLE_SECRET', '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');
